cf1f91a20e2298e339b671ad9e41b09401ae2b65
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-07-15 08:57:28 -0500

    Create Priority Interface

---
+++ Program 2/src/Priority.java
@@ -0,0 +1,17 @@
+
+public interface Priority {
+	
+// Constants
+int MED_PRIORITY = 1;
+int MAX_PRIORITY = 5;
+int MIN_PRIORITY = 10;
+
+// setPriority
+public void setPriority(int priority);
+
+// getPriority
+public int getPriority();
+}
+
+
+}
